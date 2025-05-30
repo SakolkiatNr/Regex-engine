0090ba61415f9f6ff094eb9900f537a3a0078828
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-31 02:12:45 +0700

    Add match start anywhere in a string case

--- regex.js
+++ regex.js
@@ -12,9 +12,23 @@ function matchOne(pattern, text) {
 function match(pattern, text) {
     // base case
     if (pattern === '') return true;
+    if (pattern === '$' && text === "") return true;
     else
     // invoke matchOne on successive pairs of characters from the pattern
         return (
             matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1))
         );
+}
+
+// Match the beginning of a string
+function search(pattern, text) {
+    if (pattern[0] === "^") {
+        return match(pattern.slice(1), text);
+    } else {
+        // Match somewhere in the string
+        // turn string into an array and check every letters
+        return text.split("").some((_, index) => {
+            return match(pattern, text.slice(index));
+        });
+    }
 }
