4fc5cc30b3150e53be7784700acaa02f92afe9d2
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-31 03:00:46 +0700

    Refactor codes

--- regex.js
+++ regex.js
@@ -1,8 +1,7 @@
 // Matching one character
 function matchOne(pattern, text) {
     // if empty pattern
-    if (!pattern) return true;
-    // if pattern is defined but text is empty
+    if (!pattern) return true; // if pattern is defined but text is empty
     if (!text) return false;
     if (pattern === '.') return true;
     return pattern === text;
@@ -15,6 +14,8 @@ function match(pattern, text) {
     if (pattern === '$' && text === "") return true;
     else if (pattern[1] === "?") {
         return matchQuestion(pattern, text);
+    } else if (pattern[1] === '*'){
+        return matchStar(pattern, text);
     } else {
         return (
             // invoke matchOne on successive pairs of characters from the pattern using recursive 
@@ -22,28 +23,33 @@ function match(pattern, text) {
         );
     }
 }
+
 function matchQuestion(pattern, text) {
-    // Needs to handle 2 case
+    // Needs to handle 2 cases
     // 1. Where the character before ? is not matched but the text matches the remainder of the pattern (eg. ?abc)
     // 2. Where the character before ? is matched and the rest of the text matches the remainder of the pattern (eg. a?abc)
     
     return (
-        (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1)))  || 
+        (matchOne(pattern[0], text[0]) && match(pattern, text.slice(1)))  || 
         match(pattern.slice(2), text)
     )    
 }
 
-// Match the beginning of a string
+function matchStar(pattern, text) {
+    // Needs to handle 2 cases 
+    // 1. Where the character before * is not matched but the text matches the remainder of the pattern
+    // 2. Where the character before * is matched one or more times and the rest of the text match the remainder of the pattern
+
+    return (
+        (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1)))  || 
+        match(pattern.slice(2), text)
+    ) 
+}
+
 function search(pattern, text) {
     if (pattern[0] === "^") {
         return match(pattern.slice(1), text);
     } else {
-        // Match somewhere in the string
-        // turn string into an array and check every letters
-        return text.split("").some((_, index) => {
-            return match(pattern, text.slice(index));
-        });
+        return match(".*" + pattern, text);
     }
 }
-
-search("ab?c", "abc");
